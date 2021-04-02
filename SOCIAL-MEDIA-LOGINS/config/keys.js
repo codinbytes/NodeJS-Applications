cd73d6e8e3fc38a8883536ea5e1d97bd0d796772
Saad Hassan <saad.haxxan786@gmail.com>
2021-04-02 14:19:51 +0500

    Update keys.js

--- SOCIAL-MEDIA-LOGINS/config/keys.js
+++ SOCIAL-MEDIA-LOGINS/config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   google: {
     clientID:
-      "711771466374-sbcdbte4pnkfqe9tiq78c8i7e0hjv219.apps.googleusercontent.com",
-    clientSecret: "4Vf_wwPPndSWGWsfhyVhwpYS",
+      "YOUR_CLIENT_ID",
+    clientSecret: "YOUR_CLIENT_SECRET",
   },
 };
