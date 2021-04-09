0c40b035a8ebba6922a9fe458137b9fbde6b201f
Saad Hassan <saad.haxxan786@gmail.com>
2021-04-09 20:32:15 +0500

    adding finalize version

--- SERVER-SIDE-VALIDATIONS/index.js
+++ SERVER-SIDE-VALIDATIONS/index.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
 
-app = express();
-app.set("views", "ejs");
+const app = express();
+const port = 5000;
+
+// Set Templating Enginge
+app.set("view engine", "ejs");
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get("/", (req, res) => {
+// Navigation
+app.get("", (req, res) => {
   res.render("index");
 });
 
-app.get("/register", urlencodedParser, (req, res) => {
+app.get("/register", (req, res) => {
   res.render("register");
 });
+
+app.post(
+  "/register",
+  urlencodedParser,
+  [
+    check("username", "This username must me 4+ characters long")
+      .exists()
+      .isLength({ min: 4 }),
+    check("email", "Email is not valid").isEmail().normalizeEmail(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // return res.status(422).jsonp(errors.array())
+      const alert = errors.array();
+      res.render("register", {
+        alert,
+      });
+    }
+  }
+);
+
+app.listen(port, () => console.info(`App listening on port: ${port}`));
